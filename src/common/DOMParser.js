caa0b86098aa4f5adfdc19a2dcc215c972a97680
smhg <sam.hauglustaine@gmail.com>
2021-11-05 14:19:38 +0100

    update xmldom dep

--- src/common/DOMParser.js
+++ src/common/DOMParser.js
@@ -6,4 +6,4 @@
 /* eslint-disable no-undef */
 export default typeof window !== 'undefined' && window.DOMParser
     ? window.DOMParser
-    : require('xmldom').DOMParser;
+    : require('@xmldom/xmldom').DOMParser;
